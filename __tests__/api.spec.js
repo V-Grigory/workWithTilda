3c56d9c449010d1ed7dea897ded12b710ab24c71
Grigory_Volchok <volchok@metrika72.ru>
2021-05-06 09:46:18 +0500

    refactor test

--- __tests__/api.spec.js
+++ __tests__/api.spec.js
@@ -5,77 +5,75 @@ describe('Api class', () => {
   test('checkParams() method', () => {
 
     let api
-    let errorMessage = 'When creating a tilda object, you must pass ' +
-        'projectid, publickey and secretkey'
 
     api = new Api({
       projectid: '111222',
       publickey: 'hash_publickey',
       secretkey: 'hash_secretkey'
     })
-    expect(api.checkParams().errors).toBe('')
+    expect(() => { api.checkParams() }).not.toThrow()
 
     api = new Api({
       projectid: '',
       publickey: 'hash_publickey',
       secretkey: 'hash_secretkey'
     })
-    expect(api.checkParams().errors).toBe(errorMessage)
+    expect(() => { api.checkParams() }).toThrow()
 
     api = new Api({
       publickey: 'hash_publickey',
       secretkey: 'hash_secretkey'
     })
-    expect(api.checkParams().errors).toBe(errorMessage)
+    expect(() => { api.checkParams() }).toThrow()
 
     api = new Api({
       projectid: '333333',
       publickey: '',
       secretkey: 'hash_secretkey'
     })
-    expect(api.checkParams().errors).toBe(errorMessage)
+    expect(() => { api.checkParams() }).toThrow()
 
     api = new Api({
       projectid: '111222',
       publickey: 'hash_publickey'
     })
-    expect(api.checkParams().errors).toBe(errorMessage)
+    expect(() => { api.checkParams() }).toThrow()
 
     api = new Api({
       projectid: '111222'
     })
-    expect(api.checkParams().errors).toBe(errorMessage)
+    expect(() => { api.checkParams() }).toThrow()
 
     api = new Api({
       projectid: '',
       publickey: 'hash_publickey',
       secretkey: ''
     })
-    expect(api.checkParams().errors).toBe(errorMessage)
+    expect(() => { api.checkParams() }).toThrow()
 
     api = new Api({
       secretkey: ''
     })
-    expect(api.checkParams().errors).toBe(errorMessage)
+    expect(() => { api.checkParams() }).toThrow()
 
     api = new Api({})
-    expect(api.checkParams().errors).toBe(errorMessage)
+    expect(() => { api.checkParams() }).toThrow()
 
     api = new Api()
-    expect(api.checkParams().errors).toBe(errorMessage)
+    expect(() => { api.checkParams() }).toThrow()
 
     api = new Api({
       keyA: '111222',
       keyB: 'hash_publickey'
     })
-    expect(api.checkParams().errors).toBe(errorMessage)
+    expect(() => { api.checkParams() }).toThrow()
 
     api = new Api({
       keyA: '111222',
       keyB: 'hash_publickey',
       secretkey: 'hash_secretkey'
     })
-    expect(api.checkParams().errors).toBe(errorMessage)
+    expect(() => { api.checkParams() }).toThrow()
 
   })
 
